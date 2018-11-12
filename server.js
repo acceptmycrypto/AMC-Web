13d2c8fd0ec9fa3cb6b8efcb8a9b69149cba9719
LSLNG <lawrencesuilunng@gmail.com>
2018-11-11 17:30:40 -0800

    deleted duplicate path

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var jwt = require('jsonwebtoken');
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
+  // const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
