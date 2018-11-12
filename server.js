103822818c21b70d95e1e97168352111fd6038ad
LSLNG <lawrencesuilunng@gmail.com>
2018-11-11 17:52:16 -0800

    commented out index html in server

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ var jwt = require('jsonwebtoken');
 
   // Express serve up index.html file if it doesn't recognize route
   // const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 // }
 // else app.use(express.static("public"));
 
