53d8eaa14f65c632042ad045bf64b18852f463f0
avakrishn <avakrishn@gmail.com>
2018-11-13 00:20:12 -0800

    combine sign in reducer with all reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,6 +5,7 @@ import Layout from './reducer-layout';
 import Photo from './reducer-photo';
 import LoadCrypto from './reducer-load-cryptos';
 import CryptoSelected from './reducer-sign-up';
+import SignInModal from './reducer-sign-in';
 
 const allReducers = combineReducers({
   matchedDeals: Deals,
@@ -12,7 +13,8 @@ const allReducers = combineReducers({
   Layout,
   Photo,
   LoadCrypto,
-  CryptoSelected
+  CryptoSelected,
+  SignInModal
   
 });
 
