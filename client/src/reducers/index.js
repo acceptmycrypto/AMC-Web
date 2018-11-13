61ebb018bdf83d0e3bc7b3ac31864fbaa8eb2ff2
avakrishn <avakrishn@gmail.com>
2018-11-12 18:45:58 -0800

    combine LoadCrypto reducer with all reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,16 @@
 import {combineReducers} from 'redux';
 import Deals from './reducer-deals';
 import UserInfo from './reducer-user-info';
-import Layout from './reducer-layout';
+// import Layout from './reducer-layout';
 import Photo from './reducer-photo';
+import LoadCrypto from './reducer-load-cryptos';
 
 const allReducers = combineReducers({
   matchedDeals: Deals,
-  userInfo: UserInfo,
+  UserInfo,
   Layout,
-  Photo
+  Photo,
+  LoadCrypto
   
 });
 
