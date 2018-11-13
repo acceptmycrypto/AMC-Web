8b114b677a6544d4a588fec554ba02d1cce46b5b
avakrishn <avakrishn@gmail.com>
2018-11-12 22:40:02 -0800

    add cryptoSelected SignUp reducer to all reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,18 @@
 import {combineReducers} from 'redux';
 import Deals from './reducer-deals';
 import UserInfo from './reducer-user-info';
-// import Layout from './reducer-layout';
+import Layout from './reducer-layout';
 import Photo from './reducer-photo';
 import LoadCrypto from './reducer-load-cryptos';
+import CryptoSelected from './reducer-sign-up';
 
 const allReducers = combineReducers({
   matchedDeals: Deals,
   UserInfo,
   Layout,
   Photo,
-  LoadCrypto
+  LoadCrypto,
+  CryptoSelected
   
 });
 
