559366f183669c0077784e5c2f831f73f8cf8a61
avakrishn <avakrishn@gmail.com>
2018-11-17 23:54:56 -0800

    create user component of landing page

--- client/src/containers/LandingPage/ForUser/ForUser.js
+++ client/src/containers/LandingPage/ForUser/ForUser.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import './ForUser.css';
 // Router and Route is never being called, but at the same time must not be deleted. If deleted, it thows an error.
+import Select from "react-select";
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import { connect } from "react-redux";
+import { _loadLandingDropdown } from '../../../actions/landingUserActions';
+import { handleDropdownChange } from '../../../actions/signUpActions';
+import { _addUserCryptoVotes } from '../../../services/landingUserService';
+
+
 
 class ForUser extends Component {
+  componentDidMount() {
+    this.props._loadLandingDropdown();
+  }
+
+  handleDropdownSubmit = (e) => {
+    e.preventDefault();
+
+    let email = e.target.children[0].children[1].value
+    let cryptoProfile = this.props.selectedCryptos;
+  
+    if (!email || !cryptoProfile) {
+      alert("Please enter in the required field!");
+    } else {
+      return _addUserCryptoVotes(email, cryptoProfile).then(res => {
+        console.log("message sent from server if success: ", res);
+
+      });
+    }
+  }
   render() {
+    const { error, loading, landingCryptoOptions, selectedCryptos } = this.props;
+
+    
+    if (error) {
+      return <div>Error! {error.message}</div>;
+    }
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div className="App">
         <div className="App__Aside">
@@ -18,11 +53,11 @@ class ForUser extends Component {
               <li><i className="homepage-icons fab fa-bitcoin"></i>
                 Spend Cryptocurrencies You Support
               </li>
-              <br/>
+              <br />
               <li><i className="homepage-icons fas fa-dollar-sign" aria-hidden="true"></i>
                 Find Deals from a variety of matching Vendors
               </li>
-              <br/>
+              <br />
               <li><i className="homepage-icons fas fa-chart-area" aria-hidden="true"></i>
                 Read Market Trend based on crypto purchase transactions
               </li>
@@ -34,6 +69,36 @@ class ForUser extends Component {
             <NavLink to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">For User</NavLink>
             <NavLink exact to="/vendor" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">For Vendor</NavLink>
           </div>
+          <div className="FormCenter">
+            <form className="FormFields" onSubmit={this.handleDropdownSubmit}>
+
+              <div className="FormField">
+
+                <label className="FormField__Label_landing" htmlFor="email">E-Mail Address:</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" required />
+                
+                <label className="FormField__Label_landing pt-3" htmlFor="landing_cryptos">Enter the Cryptocurrencies are you interested in spending online:</label>
+                <Select
+                  id="landing_cryptos"
+                  required
+                  onChange={this.props.handleDropdownChange}
+                  options={landingCryptoOptions}
+                  isMulti={true}
+                  autoBlur={false}
+
+                />
+
+
+              </div>
+
+              <div className="FormField">
+
+                <button className="FormField__Button mr-10">Submit</button>
+
+              </div>
+
+            </form>
+          </div>
         </div>
       </div>
 
@@ -43,12 +108,17 @@ class ForUser extends Component {
 }
 
 const mapStateToProps = state => ({
+  landingCryptoOptions: state.LandingCryptos.landingCryptoOptions,
+  loading: state.LandingCryptos.loading,
+  error: state.LandingCryptos.error, 
+  selectedCryptos: state.CryptoSelected.selectedCryptos
 
 });
 
-const matchDispatchToProps = dispatch =>{
-  return bindActionCreators({}, dispatch);
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators({ _loadLandingDropdown, handleDropdownChange }, dispatch);
 }
 
 
 export default connect(mapStateToProps, matchDispatchToProps)(ForUser);
+
