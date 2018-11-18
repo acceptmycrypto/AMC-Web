07269b958bac1c805d713e7dbb33a925f395d674
avakrishn <avakrishn@gmail.com>
2018-11-17 23:57:25 -0800

    add compose Enhansers for chrome redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import BrowserRouter from "react-router-dom/BrowserRouter";
 
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from "redux-thunk";
 import allReducers from './reducers';
 
 //all data store in variable store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//all data store in variable store
 const store = createStore(
-  allReducers,
-  applyMiddleware(thunk) //middleware which we can add to Redux to effectively teach it how to deal with new kinds of actions.
+  allReducers, composeEnhancers(applyMiddleware(thunk))
+//middleware which we can add to Redux to effectively teach it how to deal with new kinds of actions.
 );
 
 ReactDOM.render(
