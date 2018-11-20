0c0314d59e7442a9748cbb198eb55204827753cf
avakrishn <avakrishn@gmail.com>
2018-11-19 16:18:48 -0800

    create routes for results landing page

--- routes/landing_results.js
+++ routes/landing_results.js
@@ -46,16 +46,8 @@ router.get('/landing/results', function (req, res) {
 
     connection.query('SELECT landing_users_cryptos.landing_cryptos_id, landing_cryptos.crypto_name, COUNT(*) as Count FROM landing_users_cryptos LEFT JOIN landing_cryptos ON landing_users_cryptos.landing_cryptos_id = landing_cryptos.id WHERE landing_users_cryptos.landing_cryptos_id > 0 GROUP BY landing_users_cryptos.landing_cryptos_id ORDER BY Count DESC LIMIT 10;',
      function (error, results, fields) {
-        if (error) throw error;
-        var data = [];
-        var categories =[];
 
-        for(var i =0; i < results.length; i++){
-            categories.push(results[i].crypto_name);
-            data.push(results[i].Count)
-        }
-
-        res.json({categories, data});
+        res.json(results);
     });
 });
 
