afdbde77f5168060a5019e37f42d782e9c7ed2fd
Simon Nguyen <simonnguyen3054@gmail.com>
2018-11-21 11:09:38 -0800

    redirect to https

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
+  //redirect to https
+  app.get('*',function(req,res,next){
+    if(req.headers['x-forwarded-proto']!='https')
+      res.redirect(process.env.BACKEND_URL + req.url)
+    else
+      next() /* Continue to other routes if we're not redirecting */
+  })
+
   // Express serve up index.html file if it doesn't recognize route
   // const path = require('path');
   // app.get('*', (req, res) => {
@@ -38,8 +46,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   next();
 });
-
-
 
 //routers
 var navbarRoutes = require("./routes/navbar.js");
