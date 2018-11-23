d3fb053251b85801fe1fe3c3ec708eb748908338
avakrishn <avakrishn@gmail.com>
2018-11-22 21:59:05 -0800

    update crypto Portfolio reducer with Hide_Qr and Show_Qr actions

--- client/src/reducers/reducer-crypto-portfolio.js
+++ client/src/reducers/reducer-crypto-portfolio.js
@@ -2,14 +2,16 @@ import {
     SHOW_ALL_INTERESTED_CRYPTO,
     SHOW_ALL_OWNED_CRYPTO,
     HIDE_ADDRESS_FORM,
-    SHOW_ADDRESS_FORM
+    SHOW_ADDRESS_FORM,
+    HIDE_QR,
+    SHOW_QR
 
 } from "../actions/cryptoPortfolioActions";
 
 const initialUserState = {
     crypto_view: "owned",
     address_form_shown: false,
-    isQRShown: false,
+    qr_shown: false,
     users_cryptos_id: null,
     current_crypto_name: null
 }
@@ -21,17 +23,17 @@ export default function UserReducer(state = initialUserState, action) {
                 ...state,
                 crypto_view: action.payload.crypto_view,
                 address_form_shown: action.payload.address_form_shown,
-                isQRShown: action.payload.isQRShown,
+                qr_shown: action.payload.qr_shown,
                 users_cryptos_id: action.payload.users_cryptos_id,
                 current_crypto_name: action.payload.current_crypto_name
-                
+
             }
         case SHOW_ALL_OWNED_CRYPTO:
             return {
                 ...state,
                 crypto_view: action.payload.crypto_view,
                 address_form_shown: action.payload.address_form_shown,
-                isQRShown: action.payload.isQRShown,
+                qr_shown: action.payload.qr_shown,
                 users_cryptos_id: action.payload.users_cryptos_id,
                 current_crypto_name: action.payload.current_crypto_name
             }
@@ -46,6 +48,16 @@ export default function UserReducer(state = initialUserState, action) {
                 address_form_shown: action.payload.address_form_shown,
                 users_cryptos_id: action.payload.users_cryptos_id,
                 current_crypto_name: action.payload.current_crypto_name
+            }
+        case HIDE_QR:
+            return {
+                ...state,
+                qr_shown: action.payload.qr_shown
+            }
+        case SHOW_QR:
+            return {
+                ...state,
+                qr_shown: action.payload.qr_shown
             }
         default:
             return state;
