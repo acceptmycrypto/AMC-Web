7daa9d17dd3f3849f34579c82c2b1ddcaf0c221a
avakrishn <avakrishn@gmail.com>
2018-11-27 19:27:47 -0800

    add transaction status to Profile Feed Card component

--- client/src/components/Profile/FeedCard/FeedCard.js
+++ client/src/components/Profile/FeedCard/FeedCard.js
@@ -21,10 +21,28 @@ const FeedCard = props => {
               {/* <div className="user-name">{transaction.username}</div> */}
             </div>
             <div className="user-transaction-info">
-              <div>
-                Purchased <Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> for <span className="greenText">{transaction.amount + " "}</span> 
-                <span className="greenText">{transaction.crypto_symbol}</span> from <span className="blueText">{transaction.venue_name}</span>
-              </div>
+              {transaction.status == 100 && <div>
+                <div>Purchased <Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link></div> 
+                <div>For <span className="greenText">{transaction.amount + " "}</span><span className="greenText">{transaction.crypto_symbol}</span></div> 
+                <div>From <span className="blueText">{transaction.venue_name}</span></div>
+              </div>}
+              {transaction.status == 0 && <div>
+                <div>Awaiting Payment: Send <span className="greenText">{transaction.amount + " "}</span> <span className="greenText">{transaction.crypto_symbol + " "}</span></div> 
+                <div>To Address: <span className="greenText">{" " + transaction.address + " "}</span></div> 
+                <div>For <Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> from <span className="blueText">{transaction.venue_name}</span></div>
+              </div>}
+              {transaction.status == 1 && <div>
+                <div>Processing Payment:</div> 
+                <div><Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> for <span className="greenText">{transaction.amount + " "}</span> 
+                <span className="greenText">{transaction.crypto_symbol}</span> from <span className="blueText">{transaction.venue_name}</span></div>
+              </div>}
+              {transaction.status == -1 && <div>
+                <div className="redText">ORDER CANCELLED:</div> 
+                <div><Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> for <span className="greenText">{transaction.amount + " "}</span> 
+                <span className="greenText">{transaction.crypto_symbol}</span> from <span className="blueText">{transaction.venue_name}</span></div>
+              </div>}
+
+             <div><small className={'mt-3'}>Order# {transaction.txn_id}</small></div>
               <div className="timestamp">
                 {/* <small><Timestamp time={transaction.date_purchased}  format='ago' precision={2} autoUpdate={60}/></small> */}
                 <small><Timestamp time={transaction.date_purchased} format='full' precision={3}/></small>                
