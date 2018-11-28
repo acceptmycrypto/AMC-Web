42d06da9bc6797f21af6514215b2607dddc2ba08
avakrishn <avakrishn@gmail.com>
2018-11-27 19:30:30 -0800

    add order history and pending orders buttons with functionality

--- client/src/containers/Profile/ProfileFeed/ProfileFeed.js
+++ client/src/containers/Profile/ProfileFeed/ProfileFeed.js
@@ -5,6 +5,7 @@ import FeedCard from "../../../components/Profile/FeedCard";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import { _loadDeals } from "../../../actions/dealsActions";
+import { changeTxHistoryView } from '../../../actions/userLoadActions';
 
 
 class ProfileFeed extends Component {
@@ -23,53 +24,66 @@ class ProfileFeed extends Component {
     }
     shuffle = (array) => {
         var currentIndex = array.length, temporaryValue, randomIndex;
-      
+
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-      
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
-      
+
         return array;
     }
 
 
     render() {
-        const { deals } = this.props
+        const { deals, transactions, confirmed, pending, tx_history_view, changeTxHistoryView } = this.props
 
-        const dealsRecommended = this.shuffle(deals).slice(0,4);
+        const dealsRecommended = this.shuffle(deals).slice(0, 4);
+
         return (
             <div className="w-100">
-                {(this.props.transactions !== undefined && this.props.transactions.length > 0)
+                {(transactions !== undefined && transactions.length > 0)
                     ? <div>
                         <div className="mb-3 mt-1 text-center">
                             {/* <span><button className="btn btn-outline-primary" id="order-history-button"className="feed-button button-active">Order History</button><button id="" className="feed-button">Pending Orders</button></span> */}
                             <div class="btn-group" role="group" aria-label="button-group">
-                                <button type="button" class="btn btn-primary">Order History</button>
-                                <button type="button" class="btn btn-outline-primary">Pending Orders</button>
+                                {tx_history_view === "pending"
+                                    ? <div>
+                                        {confirmed.length >0 &&
+                                        <button id="orderHistoryBtn" type="button" class="btn btn-outline-primary" onClick={(event)=>{changeTxHistoryView(event, "confirmed", localStorage.getItem('token'))}}>Order History</button>}
+                                        <button id="pendingBtn"type="button" class="btn btn-primary" disabled>Pending Orders</button>
+                                    </div>
+                                    : <div>
+                                        <button id="orderHistoryBtn" type="button" class="btn btn-primary" disabled>Order History</button>
+                                        <button id="pendingBtn"type="button" class="btn btn-outline-primary " onClick={(event)=>{changeTxHistoryView(event, "pending", localStorage.getItem("token"))}}>Pending Orders</button>
+                                    </div>
+                                }
                             </div>
                         </div>
                         <div className="overflow-y">
-                            <FeedCard transactions={this.props.transactions} />
+                            {tx_history_view === "pending"
+                                ? <FeedCard transactions={pending} orderType={"pending"}/>
+                                : <FeedCard transactions={confirmed} orderType={"confirmed"} />
+                            } 
                         </div>
                     </div>
                     : <div>
                         <h2 className="color-blueRanking text-center">Recommended Deals For You</h2>
-                        <div id="right" className="grid">
+                        <div id="right" className="profileGrid">
                             {deals != undefined && deals.length > 0 && dealsRecommended.map(deal => (
-                                <div key={deal.id} className="deal">
+                                <div key={deal.id} className="profileDeal">
                                     <Link to={`/feed/deals/${deal.deal_name}`} style={{ textDecoration: 'none', color: "black" }} >
 
                                         <div className="deal-info">
                                             <img className="deal-image" src={deal.featured_deal_image} alt="deal" />
-                                            <div className="mt-1">{deal.deal_name}</div>
+                                            <div className="mt-3">{deal.deal_name}</div>
                                             <small className="deal-description">{this.handleLongDescription(deal.deal_description)}</small>
                                             <div><small>Offered by: {deal.venue_name}</small></div>
                                         </div>
@@ -108,11 +122,15 @@ const mapStateToProps = state => ({
     deals: state.matchedDeals.deals,
     loading: state.matchedDeals.loading,
     error: state.matchedDeals.error,
+    transactions: state.UserInfo.transactions,
+    confirmed: state.UserInfo.confirmed,
+    pending: state.UserInfo.pending,
+    tx_history_view: state.UserInfo.tx_history_view,
 });
 
 const matchDispatchToProps = dispatch => {
 
-    return bindActionCreators({ _loadDeals }, dispatch);
+    return bindActionCreators({ _loadDeals, changeTxHistoryView }, dispatch);
 
 }
 
