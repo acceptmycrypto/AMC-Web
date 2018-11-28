42a7e720f0cbd1b0d602a5ff40b0702bc1a3074f
avakrishn <avakrishn@gmail.com>
2018-11-27 19:24:28 -0800

    add action to change the txn_history_view in state

--- client/src/actions/userLoadActions.js
+++ client/src/actions/userLoadActions.js
@@ -1,6 +1,9 @@
 export const FETCH_USER_BEGIN = "FETCH_USER_BEGIN";
 export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
 export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+export const FETCH_TRANSACTIONS_BEGIN = "FETCH_TRANSACTIONS_BEGIN";
+export const FETCH_TRANSACTIONS_SUCCESS = "FETCH_TRANSACTIONS_SUCCESS";
+export const FETCH_TRANSACTIONS_FAILURE = "FETCH_TRANSACTIONS_FAILURE";
 
 export function _loadProfile(token) {
   const Usersettings = {
@@ -68,3 +71,54 @@ export const fetchUserFailure = error => ({
   payload: { error }
 });
 
+export const changeTxHistoryView = (event, new_tx_state, token) =>{
+     event.preventDefault();
+
+     const settings = {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({token})
+    };
+  
+    return dispatch => {
+      dispatch(fetchTransactionsBegin());
+      return fetch("/profile/user/transactions", settings)
+        .then((res) => res.json())
+        .then((transactions) => {
+          if (transactions !== undefined && transactions.length > 0) {
+            let confirmed = transactions.filter(order => order.payment_received === 1);
+            let pending = transactions.filter(order => order.payment_received === 0);
+            return([transactions, confirmed, pending]);
+            
+          }else{
+            let confirmed  = [];
+            let pending = [];
+            return([transactions, confirmed, pending]);
+          }
+        }).then(([transactions, confirmed, pending])=>{
+
+          console.log(transactions, confirmed, pending, new_tx_state);
+  
+          dispatch(fetchTransactionsSuccess(transactions, confirmed, pending, new_tx_state));
+          return (transactions, confirmed, pending, new_tx_state);
+        })
+        .catch(error => dispatch(fetchTransactionsFailure(error)));
+    };
+}
+
+export const fetchTransactionsBegin = () => ({
+  type: FETCH_TRANSACTIONS_BEGIN
+});
+
+export const fetchTransactionsSuccess = (transactions, confirmed, pending, tx_history_view) => ({
+  type: FETCH_TRANSACTIONS_SUCCESS,
+  payload: { transactions, confirmed, pending, tx_history_view }
+});
+
+export const fetchTransactionsFailure = error => ({
+  type: FETCH_TRANSACTIONS_FAILURE,
+  payload: { error }
+});
