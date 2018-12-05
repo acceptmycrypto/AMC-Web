79670c5dcbb786a44d54652434f5777ea78ef94d
avakrishn <avakrishn@gmail.com>
2018-12-05 13:28:54 -0800

    create update email post route to update user email address

--- routes/settings.js
+++ routes/settings.js
@@ -8,6 +8,15 @@ var bodyParser = require('body-parser');
 
 var verifyToken = require("./utils/validation");
 
+//use sendgrid
+var sgMail = require("@sendgrid/mail");
+var keys = require("../key");
+sgMail.setApiKey(keys.sendgrid);
+//email template
+var path = require("path");
+var fs = require('fs');
+var ejs = require('ejs');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -37,6 +46,15 @@ var connection = mysql.createConnection({
     database: process.env.DB_DB
 });
 
+//compile email template
+var newEmailTemplateText = fs.readFileSync(path.join(__dirname, '../views/emailTemplates/emailVerification/emailVerification.ejs'), 'utf-8');
+var newEmailTemplate = ejs.compile(newEmailTemplateText);
+
+var previousEmailTemplateText = fs.readFileSync(path.join(__dirname, '../views/emailTemplates/previousEmail/previousEmail.ejs'), 'utf-8');
+var previousEmailTemplate = ejs.compile(previousEmailTemplateText);
+
+var resetEmailTemplateText = fs.readFileSync(path.join(__dirname, '../views/emailTemplates/resetEmail/resetEmail.ejs'), 'utf-8');
+var resetEmailTemplate = ejs.compile(resetEmailTemplateText);
 
 
 router.post('/update/photo', verifyToken, function (req, res) {
@@ -64,12 +82,89 @@ router.post('/update/username', verifyToken, function (req, res) {
                 res.json(results);
             });
         }else{
-            res.json({responseMessage: "Username is taken. Enter a different Username"});
+            res.json({responseMessage: "Username is taken. Enter a different Username."});
         }
         
     });
 });
 
+router.post('/update/email', verifyToken, function (req, res) {
+    // var decoded = jwt.decode(token);
+    // console.log(decoded);
+    let id = req.decoded._id;
+    let email = req.body.newEmail;
+    connection.query('SELECT email FROM users WHERE ?;', [{email}], function (error, results, fields) {
+        if (error) throw error;
+        if(results.length < 1){
+            connection.query('SELECT email, email_verification_token FROM users WHERE ?;', [{id}], function (error, results, fields) {
+                if (error) throw error;
+                let previous_email = results[0].email;
+                let email_verification_token = results[0].email_verification_token
+                // console.log("Line 84", results[0].email)
+                
+                connection.query('UPDATE users SET ? , verified_email = 0 WHERE ? ;', [{email}, {id}], function (error, results, fields) {
+                    if (error) throw error;
+
+                    let verify_link = process.env.BACKEND_URL+"/email-verify/" + id + "/" + email_verification_token;
+
+                    const email_verification = {
+                        to: email,
+                        from: process.env.CUSTOMER_SUPPORT,
+                        subject: 'Confirm your email address',
+                        html: newEmailTemplate({ email, verify_link })
+                    };
+                    sgMail.send(email_verification);
+
+                    connection.query('UPDATE users SET ? WHERE ? ;', [{previous_email}, {id}], function (error, results, fields) {
+                        if (error) throw error;
+    
+                        let reverse_email_link = process.env.BACKEND_URL+"/email-reverse/" + id + "/" + email_verification_token;
+    
+                        const previous_email_template = {
+                            to: previous_email,
+                            from: process.env.CUSTOMER_SUPPORT,
+                            subject: 'Successful Email Address Change',
+                            html: previousEmailTemplate({ email, reverse_email_link })
+                        };
+                        sgMail.send(previous_email_template);
+                        
+                        res.json({responseMessage: `Check your ${email} email and confirm your new email address.`});
+                    });
+
+                });
+                
+            });
+        }else{
+            res.json({responseMessage: "Email Address is linked to another account. Enter a different Email Address."});
+        }
+        
+    });
+});
+
+router.get('/email-reverse/:user_id/:email_verification_token', function(req, res) {
+    connection.query(
+      'SELECT * FROM users WHERE id = ?',
+      [req.params.user_id],
+      function(error, result, fields) {
+        if (error) throw error;
+        let email = result[0].previous_email
+        connection.query('UPDATE users SET email = ?, previous_email = NULL, verified_email = 1 WHERE  id = ?;', [email, req.params.user_id], function (error, results, fields) {
+            if (error) throw error;
+            const email_reset_success = {
+                to: email,
+                from: process.env.CUSTOMER_SUPPORT,
+                subject: 'Successful Email Address Reset',
+                html: resetEmailTemplate({ email})
+            };
+            sgMail.send(email_reset_success);
+            
+            res.send(`Your Email Address associated with your AcceptMyCryptoAccount has been reset to ${email}.`);
+        });
+        
+  
+      }
+    );
+  });
 
 
 module.exports = router;
