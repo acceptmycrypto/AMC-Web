ea97bc95680a629397f993376bfb88cbf9003429
avakrishn <avakrishn@gmail.com>
2018-12-04 16:23:00 -0800

    create _changePhoto and _changeUsername actions

--- client/src/actions/settingsActions.js
+++ client/src/actions/settingsActions.js
@@ -1,5 +1,6 @@
 export const SET_ACTIVE_SETTINGS = "SET_ACTIVE_SETTINGS";
 export const SET_ACTIVE_PROFILE_SETTINGS = "SET_ACTIVE_PROFILE_SETTINGS";
+export const PHOTO_CHANGED = "PHOTO_CHANGED";
 
 export const handleSettingsMenuItemClick = (e, {name} ) => {
 
@@ -19,3 +20,39 @@ export const handleProfileSettingsMenuItemClick = (e, {name} ) => {
     }
 }
 
+export const _changePhoto = (token, selectedPhoto)=>{
+    const settings = {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token, selectedPhoto })
+    }; 
+    const update_photos =  fetch("/update/photo", settings)
+
+    return {
+        type: 'FETCH_PHOTO_SUCCESS',
+        payload: {
+            photo: selectedPhoto
+        }
+    }
+
+}
+
+export const _changeUsername = (token, newUsername)=>{
+    const settings = {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token, newUsername })
+    }; 
+    return fetch("/update/username", settings)
+        .then(res => res.json())
+        .then(jsonUsername => {
+            console.log("jsonUsername ",  jsonUsername);
+            return jsonUsername;
+        })
+}
