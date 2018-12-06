d680ddbb6b525351d611a9ce88374670de7b2a90
avakrishn <avakrishn@gmail.com>
2018-12-06 13:02:23 -0800

    update all transaction left action in reducers

--- client/src/containers/UserSettings/TransactionsSettings/TransactionsSettings.js
+++ client/src/containers/UserSettings/TransactionsSettings/TransactionsSettings.js
@@ -1,31 +1,21 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./TransactionsSettings.css";
-import FeedCard from "../../../components/Profile/FeedCard";
-import { _loadAllTransactions } from "../../../services/SettingsService";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Timestamp from 'react-timestamp';
+import { Link } from "react-router-dom";
+import { _allTransactions } from "../../../actions/settingsActions";
+import { _loadProfile } from "../../../actions/userLoadActions";
+
+
 
 
 class TransactionsSettings extends Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            transactions: []
-
-        };
-    }
-
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
     componentDidMount() {
-        return _loadAllTransactions(localStorage.getItem('token')).then(res => {
-        
-      
-            let {  transactions } = res;
+        // this.props._allTransactions(localStorage.getItem('token'));
+        this.props._loadProfile(localStorage.getItem("token"));
 
-           
-            this.setState({ transactions });
-            
-          });
     }
     render() {
 
@@ -33,15 +23,72 @@ class TransactionsSettings extends Component {
         return (
             <div className="w-100 mx-0 text-center">
 
-                    <h1 className="text-center lightBlueText"> All Transactions History </h1>
+                <h1 className="text-center lightBlueText"> All Transactions History </h1>
 
-                    <div className="w-50 mx-auto">
-                    <FeedCard transactions={this.state.transactions} />
-                    </div>
+                <div className="w-75 mx-auto">
+
+                    {this.props.transactions.map((transaction, i) => (
+                        <div key={i} className="feed-card ">
+                            <div className="card-content">
+                                <div className="user-info">
+                                    <div className="mt-3">
+                                        {/* {(transaction.photo.indexOf("fa-user") !== -1)
+                                                ? <i className={'fas py-2 px-2 user-icon-shaded-small ' + transaction.photo}></i>
+                                                 : <img src={transaction.photo}></img>
+                                        } */}
+                                        <img className="featured-image" src={transaction.featured_deal_image} width="100%"></img>
+                                        {/* <i class="user-icon fas fa-user-circle" /> */}
+                                    </div>
+                                    {/* <div className="user-name">{transaction.username}</div> */}
+                                </div>
+                                <div className="user-transaction-info text-left">
+                                    {transaction.status === "100" && <div>
+                                        <div>Purchased <Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link></div>
+                                        <div>For <span className="greenText">{transaction.amount + " "}</span><span className="greenText">{transaction.crypto_symbol}</span></div>
+                                        <div>From <span className="blueText">{transaction.venue_name}</span></div>
+                                    </div>}
+                                    {transaction.status === "1" && <div>
+                                        <div>Processing Payment:</div>
+                                        <div><Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> for <span className="greenText">{transaction.amount + " "}</span>
+                                            <span className="greenText">{transaction.crypto_symbol}</span> from <span className="blueText">{transaction.venue_name}</span></div>
+                                    </div>}
+                                    {transaction.status === "0" && <div>
+                                        <div>Awaiting Payment: Send <span className="greenText">{transaction.amount + " "}</span> <span className="greenText">{transaction.crypto_symbol + " "}</span></div>
+                                        <div>To Address: <span className="greenText">{" " + transaction.address + " "}</span></div>
+                                        <div>For <Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> from <span className="blueText">{transaction.venue_name}</span></div>
+                                    </div>}
+                                    {transaction.status === "-1" && <div>
+                                        <div className="redText">ORDER CANCELLED:</div>
+                                        <div><Link to={`/feed/deals/${transaction.deal_name}`} ><span className="blueText">{transaction.deal_name}</span></Link> for <span className="greenText">{transaction.amount + " "}</span>
+                                            <span className="greenText">{transaction.crypto_symbol}</span> from <span className="blueText">{transaction.venue_name}</span></div>
+                                    </div>}
+
+                                    <div><small className={'mt-3'}>Order# {transaction.txn_id}</small></div>
+                                    <div className="timestamp">
+                                        {/* <small><Timestamp time={transaction.date_purchased}  format='ago' precision={2} autoUpdate={60}/></small> */}
+                                        <small><Timestamp time={transaction.date_purchased} format='full' precision={3} /></small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
             </div>
         );
     }
 }
 
 
-export default TransactionsSettings;
+const mapStateToProps = state => ({
+    // transactionInfo: state.Settings.transactionInfo
+    transactions: state.UserInfo.transactions,
+
+});
+
+const matchDispatchToProps = dispatch => {
+    return bindActionCreators({ _loadProfile }, dispatch);
+}
+
+
+export default connect(mapStateToProps, matchDispatchToProps)(TransactionsSettings);
