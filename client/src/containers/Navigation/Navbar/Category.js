5d3cdae5ee8d87a2f659f846bf680f07edbcbc84
Simon Nguyen <simonnguyen3054@gmail.com>
2018-12-06 16:00:32 -0800

    create a unique array for the filtered category

--- client/src/containers/Navigation/Navbar/Category.js
+++ client/src/containers/Navigation/Navbar/Category.js
@@ -5,6 +5,21 @@ import {filterCategory} from '../../../actions/categoryActions';
 
 class Category extends Component {
 
+    sortCategory = (deals) => {
+      let newCategory = [];
+      deals.map(deal => (
+        newCategory.push(deal.category)
+      ))
+
+      let uniqueArray = [...new Set(newCategory)]
+
+      return uniqueArray
+
+      // let filteredCategory = newCategory.filter(function(category, pos){
+      //   return newCategory.indexOf(category)== pos;
+      // });
+    }
+
     render(){
         return (
           <div className="dropdown show mx-4">
@@ -12,9 +27,15 @@ class Category extends Component {
               Categories
             </a>
             <div className="dropdown-menu category-dropdown" aria-labelledby="dropdownMenuLink">
-              {this.props.allDeals.map(deal => (
+            {this.sortCategory(this.props.allDeals).map((category, i) => {
+              return (
+               <div key={i} className="dropdown-item" onClick={(event) => this.props.filterCategory(event.target.innerHTML, this.props.allDeals)}>{category}</div>)
+            })}
+
+
+              {/* {this.props.allDeals.map(deal => (
                 <div key={deal.id} className="dropdown-item" onClick={(event) => this.props.filterCategory(event.target.innerHTML, this.props.allDeals)}>{deal.category}</div>
-              ))}
+              ))} */}
             </div>
           </div>
         )
