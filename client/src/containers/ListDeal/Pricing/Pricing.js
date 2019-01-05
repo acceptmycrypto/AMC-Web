97269427e5d892954bfa0b9e9c1e39eedb8ebf17
Simon Nguyen <simonnguyen3054@gmail.com>
2019-01-04 20:02:06 -0800

    add crypto payment container

--- client/src/containers/ListDeal/Pricing/Pricing.js
+++ client/src/containers/ListDeal/Pricing/Pricing.js
@@ -2,65 +2,71 @@ import React, { Component } from "react";
 import "./Pricing.css";
 
 const Pricing = props => {
-  // const {} = props;
-
   return (
-    <div className="add-price-for-deal">
-      <div className="listed-price-in-dollar">
-        <div className="pricing-titles">Price in Dollar</div>
-        <div className="pricing-input-row">
-          <i class="fas fa-dollar-sign fa-2x" />
-          <input
-            autofocus="autofocus"
-            className="pricing-input"
-            type="number"
-            placeholder="Enter your base price"
-            min="0"
-            onChange={props.handlePriceUSDChange}
-            value={props.showPriceUSD}
-            onBlur={props.validateBasePrice}
-          />
-        </div>
-        <small className="pricing-footer-note">
-          This is your price if buyer pay in USD.
-        </small>
-      </div>
-      <div className="listed-price-in-crypto">
-        <div className="listed-price-in-crypto-left">
-          <div className="pricing-titles">Price in Cryptocurrency</div>
+    <div>
+      <div className="add-price-for-deal">
+        <div className="listed-price-in-dollar">
+          <div className="pricing-titles">Price in Dollar</div>
           <div className="pricing-input-row">
             <i class="fas fa-dollar-sign fa-2x" />
             <input
-              className="pricing-input-crypto"
-              type="text"
-              placeholder="0.00"
+              autofocus="autofocus"
+              className="pricing-input"
+              type="number"
+              placeholder="Enter your base price"
               min="0"
-              value={props.showPriceCrypto}
+              onChange={props.handlePriceUSDChange}
+              value={props.showPriceUSD}
+              onBlur={props.validateBasePrice}
             />
           </div>
           <small className="pricing-footer-note">
-            This is your price if buyer pay in Cryptocurrency.
+            This is your price if buyer pay in USD.
           </small>
         </div>
-        <div className="listed-price-in-crypto-right">
-          <div className="create-deal-discount-percent">
-            {props.showDiscountPercent}% OFF
-          </div>
-          <input
-            onChange={props.changeDiscountPercent}
-            type="range"
-            min="10"
-            max="100"
-            defaultValue={props.showDiscountPercent}
-            value={props.showDiscountPercent}
-            id="percent-slider"
-          />
-          <div className="pricing-slider-footer-note">
-            <div className="discount-pricing-title">Set your discount</div>
-            <small>
-              Minimum 10% off is required.
+        <div className="listed-price-in-crypto">
+          <div className="listed-price-in-crypto-left">
+            <div className="pricing-titles">Price in Cryptocurrency</div>
+            <div className="pricing-input-row">
+              <i class="fas fa-dollar-sign fa-2x" />
+              <input
+                className="pricing-input-crypto"
+                type="text"
+                placeholder="0.00"
+                min="0"
+                value={props.showPriceCrypto}
+              />
+            </div>
+            <small className="pricing-footer-note">
+              This is your price if buyer pay in Cryptocurrency.
             </small>
           </div>
+          <div className="listed-price-in-crypto-right">
+            <div className="create-deal-discount-percent">
+              {props.showDiscountPercent}% OFF
+            </div>
+            <input
+              onChange={props.changeDiscountPercent}
+              type="range"
+              min="10"
+              max="100"
+              defaultValue={props.showDiscountPercent}
+              value={props.showDiscountPercent}
+              id="percent-slider"
+            />
+            <div className="pricing-slider-footer-note">
+              <div className="discount-pricing-title">Set your discount</div>
+              <small>
+                Minimum 10% off is required.
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="select-crypto-for-deal">
+        <div>
+          <div className="pricing-titles">Accept Cryptocurrencies</div>
+          <div className="crypto-subtitle">Select one or more cryptocurrencies you want to accept as payment</div>
         </div>
       </div>
     </div>
