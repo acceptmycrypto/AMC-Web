19ca643d0289dbc1a8ff0016316d3f55a453e26c
avakrishn <avakrishn@gmail.com>
2019-01-06 17:39:43 -0800

    create shipment routes file

---
+++ routes/shipment.js
@@ -0,0 +1,42 @@
+var mysql = require("mysql");
+var express = require('express');
+var app = express();
+var router = express.Router();
+var methodOverride = require('method-override');
+
+var bodyParser = require('body-parser');
+
+var verifyToken = require("./utils/validation");
+
+var shippo = require('shippo')(process.env.SHIPMENT_KEY);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(bodyParser.json());
+
+var path = require("path");
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+
+
+
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: process.env.DB_USER,
+
+    // Your password
+    password: process.env.DB_PW,
+    database: process.env.DB_DB
+});
