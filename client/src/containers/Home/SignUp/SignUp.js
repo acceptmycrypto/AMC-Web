78bb10f1b1d4265ff598458f2133756ca65ca132
avakrishn <avakrishn@gmail.com>
2019-01-15 23:57:42 -0800

    add Aside component

--- client/src/containers/Home/SignUp/SignUp.js
+++ client/src/containers/Home/SignUp/SignUp.js
@@ -10,6 +10,7 @@ import { _loadCryptocurrencies } from "../../../actions/loadCryptoActions";
 import { handleDropdownChange, openModal, closeModal, resetSelectedCryptos } from "../../../actions/signUpActions";
 
 import Footer from "../../../components/Layout/Footer";
+import Aside from "../Aside";
 
 
 class SignUp extends Component {
@@ -83,33 +84,12 @@ class SignUp extends Component {
     // }
     return (
       <div className="App">
-        <div className="App__Aside">
-          <img className="crypto-img img-fluid mb-5 d-block mx-auto" src="../../../assets/images/logo.png" alt=""></img>
-          <h1 className="text-uppercase mb-0 ">Accept My Crypto</h1>
-          <hr className="star-light"></hr>
-          <h2 className="font-weight-light mb-0">
-            <ul>
-              <br></br>
-              <li><i className="homepage-icons fas fa-dollar-sign"></i>
-                Grab Deals for Purchase with Cryptocurrency
-                </li>
-              <br></br>
-
-              <li><i className="homepage-icons fa fa-user" aria-hidden="true"></i>
-                Find Friends with Matching Currencies
-              </li>
-              <br></br>
-              <li><i className="homepage-icons fa fa-users" aria-hidden="true"></i>
-                Engage with Your Crypto Community
-              </li>
-            </ul>
-          </h2>
-        </div>
+        <Aside/>
         <div className="App__Form">
           <div className="PageSwitcher">
             <NavLink
               exact
-              to="/"
+              to="/SignIn"
               activeClassName="PageSwitcher__Item--Active"
               className="PageSwitcher__Item"
             >
