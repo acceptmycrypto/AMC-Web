366ce785c4578b7a07d23da30b0a60659ac32752
avakrishn <avakrishn@gmail.com>
2019-01-15 23:01:22 -0800

    check if user is logged in in navbar to either display user photo or Sign In/ Up

--- client/src/containers/Navigation/Navbar/Navbar.js
+++ client/src/containers/Navigation/Navbar/Navbar.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 import Notification from "../Notification";
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import { connect } from "react-redux";
 import { _loadPhoto } from "../../../actions/navbarActions";
 import SearchBar from "./Searchbar";
 import Category from "./Category";
 import { resetNavbar } from "../../../actions/navbarActions";
+import { _isLoggedIn } from "../../../actions/loggedInActions";
+
 
 class Navbar extends Component {
 
@@ -17,28 +19,42 @@ class Navbar extends Component {
   }
 
   componentDidMount() {
-    this.props._loadPhoto(localStorage.getItem('token'));
+
+    if (this.props.userLoggedIn) {
+      this.props._loadPhoto(localStorage.getItem('token'));
+    } else {
+
+    }
+    // let isLoggedIn = await this.props._isLoggedIn(localStorage.getItem('token'));
+    // if(isLoggedIn.message == "WrongToken"){
+
+    // }
+    // 
+
+    console.log(this.props.photo.photo);
+
+
   }
 
   render() {
     return (
       <header className="Toolbar">
         <div className="nav-left">
-          <Link onClick={this.props.resetNavbar} to="/feed/deals" className="Logo">
+          <Link onClick={this.props.resetNavbar} to="/" className="Logo">
             <div className="font-17 color-deepBlue">
-              <img className="navbar_logo" src="https://s3-us-west-1.amazonaws.com/acceptmycrypto/logo.png" alt="logo"/>
+              <img className="navbar_logo" src="https://s3-us-west-1.amazonaws.com/acceptmycrypto/logo.png" alt="logo" />
               <span className="ml-2">
-              AcceptMyCrypto
+                AcceptMyCrypto
               </span>
             </div>
           </Link>
 
-          { window.location.pathname == "/feed/deals" ?
+          {window.location.pathname == "/feed/deals" ?
             <SearchBar /> : null
           }
 
-          { window.location.pathname == "/feed/deals" ?
-            <Category/> : null
+          {window.location.pathname == "/feed/deals" ?
+            <Category /> : null
           }
 
           <div className="Feed">
@@ -52,7 +68,7 @@ class Navbar extends Component {
         <div className="Nav d-flex flex-row align-items-center">
           <li>
             <Link onClick={this.props.resetNavbar} to="/listdeal">
-              { window.location.pathname == "/listdeal"
+              {window.location.pathname == "/listdeal"
                 ? <i className="fas fas fa-store fa-lg"> <h7 className="color-deepBlue font-17 teal-underline">Create a Deal</h7></i>
                 : <i className="fas fas fa-store fa-lg"> <h7 className="color-deepBlue font-17">Create a Deal</h7></i>
               }
@@ -60,26 +76,30 @@ class Navbar extends Component {
           </li>
           <li>
             <Link onClick={this.props.resetNavbar} to="/feed/deals">
-            { window.location.pathname == "/feed/deals"
-              ? <i className="fas fa-dollar-sign fa-lg"> <h7 className="color-deepBlue font-17 teal-underline">All Deals</h7></i>
-              : <i className="fas fa-dollar-sign fa-lg"> <h7 className="color-deepBlue font-17">All Deals</h7></i>
-            }
+              {window.location.pathname == "/feed/deals"
+                ? <i className="fas fa-dollar-sign fa-lg"> <h7 className="color-deepBlue font-17 teal-underline">All Deals</h7></i>
+                : <i className="fas fa-dollar-sign fa-lg"> <h7 className="color-deepBlue font-17">All Deals</h7></i>
+              }
             </Link>
           </li>
           <li>
-            <div className="dropdown show m-0 p-0">
-              <div className="dropdown-toggle picture-toggle m-0 p-0" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i className={'fas my-1 py-2 px-3 user-icon-navbar ' + this.props.photo.photo}></i>
-
+            {this.props.photo.photo
+              ? <div className="dropdown show m-0 p-0">
+                <div className="dropdown-toggle picture-toggle m-0 p-0" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i className={'fas my-1 py-2 px-3 user-icon-navbar ' + this.props.photo.photo}></i>
+                </div>
+                <div className="dropdown-menu m-0" aria-labelledby="dropdownMenuLink">
+                  <Link className="dropdown-item" to="/profile">Profile</Link>
+                  <Link className="dropdown-item" to="/settings">Settings</Link>
+                  <Link className="dropdown-item" to="/" onClick={this.logOut} >Logout</Link>
+                </div>
               </div>
-
-              <div className="dropdown-menu m-0" aria-labelledby="dropdownMenuLink">
-                <Link className="dropdown-item" to="/profile">Profile</Link>
-                <Link className="dropdown-item" to="/settings">Settings</Link>
-                <Link className="dropdown-item" to="/" onClick={this.logOut} >Logout</Link>
+              : <div className="d-flex flex-row align-items-center">
+                <Link to="/SignIn"><p className="navbar-login">Sign In</p></Link>
+                <div><p className="clear-text-decoration"> / </p></div>
+                <Link to="/SignUp"><p className="navbar-login">Sign Up</p> </Link>
               </div>
-            </div>
-
+            }
           </li>
         </div>
       </header>
@@ -90,11 +110,12 @@ class Navbar extends Component {
 const mapStateToProps = state => ({
   photo: state.Photo,
   loading: state.Photo.loading,
-  error: state.Photo.error
+  error: state.Photo.error,
+  userLoggedIn: state.LoggedIn.userLoggedIn,
 });
 
-const matchDispatchToProps = dispatch =>{
-  return bindActionCreators({ _loadPhoto, resetNavbar }, dispatch);
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators({ _isLoggedIn, _loadPhoto, resetNavbar }, dispatch);
 }
 
 
