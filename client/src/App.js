458e0b7b1436faca9bbc063df1d710255cd569c4
avakrishn <avakrishn@gmail.com>
2019-01-24 12:36:00 -0800

    import bootstrap for reactstrap carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Switch, Route, Link, Redirect } from "react-router-dom";
-
 import SignUp from "./containers/Home/SignUp";
 import SignIn from "./containers/Home/SignIn";
 import ResendEmail from "./containers/Home/ResendEmail";
@@ -9,12 +8,9 @@ import ResetPasswordEmail from "./containers/Home/ResetPasswordEmail";
 import Profile from "./containers/Profile/UserProfile";
 import Settings from "./containers/UserSettings/Settings";
 import Homepage from "./containers/Home/Homepage";
-
-
 import FeedDeals from "./containers/Deals";
 import DealItem from "./containers/Deals/DealItem";
 import Reviews from "./containers/Reviews";
-
 import ListDeal from "./containers/ListDeal";
 
 // import Layout from "./components/Layout";
