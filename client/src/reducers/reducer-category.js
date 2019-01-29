0b6f737c6952a128f2185101e32f2f5ae75aad80
avakrishn <avakrishn@gmail.com>
2019-01-28 18:45:16 -0800

    update category reducer

--- client/src/reducers/reducer-category.js
+++ client/src/reducers/reducer-category.js
@@ -1,6 +1,10 @@
 const initialState = {
-  category: null,
-  filteredCategory: []
+    category: null,
+    filteredCategory: [],
+    categoryTerm: "",
+    categoriesDeals: [],
+    categoryPage: 1,
+    categoryNumberOfResults: 0
 };
 
 export default function categoryReducer(state = initialState, action) {
@@ -15,6 +19,14 @@ export default function categoryReducer(state = initialState, action) {
             return initialState;
         case "RESET_NAVBAR":
             return initialState;
+        case "CATEGORIES_DEALS_SUCCESS":
+            return {
+                ...state,
+                categoryTerm: action.payload.categoryTerm,
+                categoriesDeals: action.payload.categoriesDeals,
+                categoryPage: action.payload.categoryPage,
+                categoryNumberOfResults: action.payload.categoryNumberOfResults
+            };
         default:
             return state;
     }
