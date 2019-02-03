837dfae50f1d7c0615d0fa33edc8853b704672b9
avakrishn <avakrishn@gmail.com>
2019-02-02 21:30:26 -0800

    update homepage reducer to deal with all categorized deals

--- client/src/reducers/reducer-homepage.js
+++ client/src/reducers/reducer-homepage.js
@@ -2,10 +2,11 @@ import { FETCH_HOMEPAGE_DEALS_BEGIN, FETCH_HOMEPAGE_DEALS_SUCCESS, FETCH_HOMEPAG
 
 const initialState = {
     category_list : [],
-    apparel_accessories :[],
-    electronics: [], 
-    health_beauty: [], 
-    movies_music_games: []
+    homepage_deals : []
+    // apparel_accessories :[],
+    // electronics: [], 
+    // health_beauty: [], 
+    // movies_music_games: []
   };
   export default function homepageReducer(state = initialState, action) {
     switch (action.type) {
@@ -21,11 +22,12 @@ const initialState = {
         return {
           ...state,
           loading: false,
+          homepage_deals: action.payload.homepage_deals,
           category_list: action.payload.category_list,
-          apparel_accessories: action.payload.apparel_accessories,
-          electronics: action.payload.electronics,
-          health_beauty: action.payload.health_beauty, 
-          movies_music_games: action.payload.movies_music_games
+          // apparel_accessories: action.payload.apparel_accessories,
+          // electronics: action.payload.electronics,
+          // health_beauty: action.payload.health_beauty, 
+          // movies_music_games: action.payload.movies_music_games
         };
   
       case FETCH_HOMEPAGE_DEALS_FAILURE:
