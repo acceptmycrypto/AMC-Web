aa676090771ff2288915e3f1e7f25a11365a02c5
avakrishn <avakrishn@gmail.com>
2019-02-02 21:29:27 -0800

    format document

--- client/src/containers/ListDeal/UploadImage/UploadingImage.js
+++ client/src/containers/ListDeal/UploadImage/UploadingImage.js
@@ -12,39 +12,42 @@ const UploadingImage = props => {
       <div className="deal-listing-images">
         <div className="first-row">
           {/* image one */}
-          {uploadedImages[0] !== undefined ? (
-            <div
-              data-imagekey={uploadedImages[0].Key}
-              className="deal-uploaded-img-border col-3"
-            >
-              <i
-                onClick={removeImage}
-                className="fa fa-lg fa-times-circle delete-uploading-photo"
-                aria-hidden="true"
-              />
-              <img
-                onClick={viewImage}
-                className="uploaded-listing-image"
-                src={uploadedImages[0].Location}
-                alt="uploaded_image"
-              />
-              <div class="main-uploaded-photo">Featured Photo</div>
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i className="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
+          {uploadedImages[0] !== undefined 
+            ? (
+              <div
+                data-imagekey={uploadedImages[0].Key}
+                className="deal-uploaded-img-border col-3"
+              >
+                <i
+                  onClick={removeImage}
+                  className="fa fa-lg fa-times-circle delete-uploading-photo"
+                  aria-hidden="true"
+                />
+                <img
+                  onClick={viewImage}
+                  className="uploaded-listing-image"
+                  src={uploadedImages[0].Location}
+                  alt="uploaded_image"
+                />
+                <div class="main-uploaded-photo">Featured Photo</div>
+              </div>
+            ) 
+            : (
+              <div className="deal-listing-img col-3">
+                <label htmlFor="small-photo-upload">
+                  <i className="fas fa-plus fa-2x" />
+                </label>
+                <input
+                  type="file"
+                  id="small-photo-upload"
+                  onChange={uploadImage}
+                />
+              </div>
           )}
 
           {/* image two */}
-          {uploadedImages[1] !== undefined ? (
+          {uploadedImages[1] !== undefined 
+          ? (
             <div
               data-imagekey={uploadedImages[1].Key}
               className="deal-uploaded-img-border col-3"
@@ -61,58 +64,66 @@ const UploadingImage = props => {
                 alt="uploaded_image"
               />
             </div>
-          ) : uploadedImages[0] !== undefined ? (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i class="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3" />
+          ) 
+          : uploadedImages[0] !== undefined 
+            ? (
+              <div className="deal-listing-img col-3">
+                <label htmlFor="small-photo-upload">
+                  <i class="fas fa-plus fa-2x" />
+                </label>
+                <input
+                  type="file"
+                  id="small-photo-upload"
+                  onChange={uploadImage}
+                />
+              </div>
+            ) 
+            : (
+              <div className="deal-listing-img col-3" />
           )}
 
           {/* image three */}
-          {uploadedImages[2] !== undefined ? (
-            <div
-              data-imagekey={uploadedImages[2].Key}
-              className="deal-uploaded-img-border col-3"
-            >
-              <i
-                onClick={removeImage}
-                class="fa fa-lg fa-times-circle delete-uploading-photo"
-                aria-hidden="true"
-              />
-              <img
-                onClick={viewImage}
-                className="uploaded-listing-image"
-                src={uploadedImages[2].Location}
-                alt="uploaded_image"
-              />
-            </div>
-          ) : uploadedImages[1] !== undefined ? (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i class="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3" />
+          {uploadedImages[2] !== undefined 
+            ? (
+              <div
+                data-imagekey={uploadedImages[2].Key}
+                className="deal-uploaded-img-border col-3"
+              >
+                <i
+                  onClick={removeImage}
+                  class="fa fa-lg fa-times-circle delete-uploading-photo"
+                  aria-hidden="true"
+                />
+                <img
+                  onClick={viewImage}
+                  className="uploaded-listing-image"
+                  src={uploadedImages[2].Location}
+                  alt="uploaded_image"
+                />
+              </div>
+            ) 
+            : uploadedImages[1] !== undefined 
+              ? (
+                <div className="deal-listing-img col-3">
+                  <label htmlFor="small-photo-upload">
+                    <i class="fas fa-plus fa-2x" />
+                  </label>
+                  <input
+                    type="file"
+                    id="small-photo-upload"
+                    onChange={uploadImage}
+                  />
+                </div>
+              ) 
+              : (
+                <div className="deal-listing-img col-3" />
           )}
         </div>
 
         <div className="second-row">
           {/* image four */}
-          {uploadedImages[3] !== undefined ? (
+          {uploadedImages[3] !== undefined 
+          ? (
             <div
               data-imagekey={uploadedImages[3].Key}
               className="deal-uploaded-img-border col-3"
@@ -129,23 +140,27 @@ const UploadingImage = props => {
                 alt="uploaded_image"
               />
             </div>
-          ) : uploadedImages[2] !== undefined ? (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i class="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3" />
+          ) 
+          : uploadedImages[2] !== undefined 
+            ? (
+              <div className="deal-listing-img col-3">
+                <label htmlFor="small-photo-upload">
+                  <i class="fas fa-plus fa-2x" />
+                </label>
+                <input
+                  type="file"
+                  id="small-photo-upload"
+                  onChange={uploadImage}
+                />
+              </div>
+            ) 
+            : (
+              <div className="deal-listing-img col-3" />
           )}
 
           {/* image four */}
-          {uploadedImages[4] !== undefined ? (
+          {uploadedImages[4] !== undefined 
+          ? (
             <div
               data-imagekey={uploadedImages[4].Key}
               className="deal-uploaded-img-border col-3"
@@ -162,23 +177,27 @@ const UploadingImage = props => {
                 alt="uploaded_image"
               />
             </div>
-          ) : uploadedImages[3] !== undefined ? (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i class="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3" />
-          )}
+            ) 
+          : uploadedImages[3] !== undefined 
+              ? (
+                <div className="deal-listing-img col-3">
+                  <label htmlFor="small-photo-upload">
+                    <i class="fas fa-plus fa-2x" />
+                  </label>
+                  <input
+                    type="file"
+                    id="small-photo-upload"
+                    onChange={uploadImage}
+                  />
+                </div>
+              ) 
+              : (
+                <div className="deal-listing-img col-3" />
+              )}
 
           {/* image six */}
-          {uploadedImages[5] !== undefined ? (
+          {uploadedImages[5] !== undefined 
+          ? (
             <div
               data-imagekey={uploadedImages[5].Key}
               className="deal-uploaded-img-border col-3"
@@ -195,20 +214,23 @@ const UploadingImage = props => {
                 alt="uploaded_image"
               />
             </div>
-          ) : uploadedImages[4] !== undefined ? (
-            <div className="deal-listing-img col-3">
-              <label htmlFor="small-photo-upload">
-                <i class="fas fa-plus fa-2x" />
-              </label>
-              <input
-                type="file"
-                id="small-photo-upload"
-                onChange={uploadImage}
-              />
-            </div>
-          ) : (
-            <div className="deal-listing-img col-3" />
-          )}
+            ) 
+          : uploadedImages[4] !== undefined 
+            ? (
+              <div className="deal-listing-img col-3">
+                <label htmlFor="small-photo-upload">
+                  <i class="fas fa-plus fa-2x" />
+                </label>
+                <input
+                  type="file"
+                  id="small-photo-upload"
+                  onChange={uploadImage}
+                />
+              </div>
+              ) 
+            : (
+              <div className="deal-listing-img col-3" />
+            )}
         </div>
         <hr className="creating-deal-hr"/>
         <div id="photos-next-step">
