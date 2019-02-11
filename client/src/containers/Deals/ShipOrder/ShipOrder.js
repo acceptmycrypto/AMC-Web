de73a257dc2cd5d1b0c5f59afc353a1784ba11f6
avakrishn <avakrishn@gmail.com>
2019-02-10 22:15:16 -0800

    additional email and phone number inputs should be filled in from guest user

--- client/src/containers/Deals/ShipOrder/ShipOrder.js
+++ client/src/containers/Deals/ShipOrder/ShipOrder.js
@@ -2,87 +2,119 @@ import React from "react";
 import "./ShipOrder.css";
 
 const ShipOrder = props => {
-  console.log("STATUS", this.props.user_status)
+  
   return (
     <div>
       <div className="shipping-form">
         <label className="text-capitalize shipping-name-label" htmlFor="select_crypto">Enter Shipping Information</label>
         <form>
-        <div class="form-group mb-1">
-          <input
-            type="text"
-            class="form-control"
-            id="firstname"
-            placeholder="Enter First Name"
-            onChange={props.handle_ShippingFirstName}
-            value={props.showShippingFirstName ? props.showShippingFirstName : null}
-          />
-        </div>
-        <div className="dealitem-error-msg" id="shipping-firstname-error"></div>
+          <div class="form-group mb-1">
+            <input
+              type="text"
+              class="form-control"
+              id="firstname"
+              placeholder="Enter First Name"
+              onChange={props.handle_ShippingFirstName}
+              value={props.showShippingFirstName ? props.showShippingFirstName : null}
+            />
+          </div>
+          <div className="dealitem-error-msg" id="shipping-firstname-error"></div>
 
-        <div class="form-group mb-1">
-          <input
-            type="text"
-            class="form-control"
-            id="lastname"
-            placeholder="Enter Last Name"
-            onChange={props.handle_ShippingLastName}
-            value={props.showShippingLastName ? props.showShippingLastName : null}
-          />
-        </div>
-        <div className="dealitem-error-msg" id="shipping-lastname-error"></div>
+          <div class="form-group mb-1">
+            <input
+              type="text"
+              class="form-control"
+              id="lastname"
+              placeholder="Enter Last Name"
+              onChange={props.handle_ShippingLastName}
+              value={props.showShippingLastName ? props.showShippingLastName : null}
+            />
+          </div>
+          <div className="dealitem-error-msg" id="shipping-lastname-error"></div>
 
-        <div class="form-group mb-1">
-          <input
-            type="text"
-            class="form-control"
-            id="address"
-            placeholder="Enter Address"
-            onChange={props.handle_ShippingAddress}
-            value={props.showShippingAddress ? props.showShippingAddress : null}
-          />
-        </div>
-        <div className="dealitem-error-msg" id="shipping-address-error"></div>
+          <div class="form-group mb-1">
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="Enter Address"
+              onChange={props.handle_ShippingAddress}
+              value={props.showShippingAddress ? props.showShippingAddress : null}
+            />
+          </div>
+          <div className="dealitem-error-msg" id="shipping-address-error"></div>
 
-        <div class="form-group mb-1">
-          <input
-            type="text"
-            class="form-control"
-            id="address"
-            placeholder="Enter City"
-            onChange={props.handle_ShippingCity}
-            value={props.showShippingCity ? props.showShippingCity : null}
-          />
-        </div>
-        <div className="dealitem-error-msg" id="shipping-city-error"></div>
+          <div class="form-group mb-1">
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="Enter City"
+              onChange={props.handle_ShippingCity}
+              value={props.showShippingCity ? props.showShippingCity : null}
+            />
+          </div>
+          <div className="dealitem-error-msg" id="shipping-city-error"></div>
 
-        <select
-          className="custom-select mr-sm-2"
-          id="select-color"
-          onChange={props.handle_ShippingState}
-          value={props.showShippingState}
-        >
-          <option selected>Select State</option>
-          {props.listOfAllStates.map((state, i) => {
-            return (<option key={i} value={state.label}>{state.value}</option>)
-          })}
-        </select>
+          <select
+            className="custom-select mr-sm-2"
+            id="select-color"
+            onChange={props.handle_ShippingState}
+            value={props.showShippingState}
+          >
+            <option selected>Select State</option>
+            {props.listOfAllStates.map((state, i) => {
+              return (<option key={i} value={state.label}>{state.value}</option>)
+            })}
+          </select>
 
-        <div className="dealitem-error-msg" id="shipping-state-error"></div>
+          <div className="dealitem-error-msg" id="shipping-state-error"></div>
 
-        <div class="form-group mb-1">
-          <input
-            type="text"
-            class="form-control"
-            id="address"
-            placeholder="Enter Postal Code"
-            onChange={props.handle_ShippingZipcode}
-            value={props.showShippingZipcode ? props.showShippingZipcode : null}
-          />
-        </div>
-        <div className="dealitem-error-msg" id="shipping-zipcode-error"></div>
+          <div class="form-group mb-1">
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="Enter Postal Code"
+              onChange={props.handle_ShippingZipcode}
+              value={props.showShippingZipcode ? props.showShippingZipcode : null}
+            />
+          </div>
+          <div className="dealitem-error-msg" id="shipping-zipcode-error"></div>
 
-      </form>
+
+          {props.user_status === "guest" &&
+            <div>
+              <div class="form-group mb-1">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  placeholder="Enter Email"
+                  onChange={props.handle_ShippingEmail}
+                  value={props.showShippingEmail ? props.showShippingEmail : null}
+                />
+              </div>
+              <div className="dealitem-error-msg" id="shipping-email-error"></div>
+
+              <div class="form-group mb-1">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="phone_number"
+                  placeholder="Enter Phone Number"
+                  onChange={props.handle_ShippingPhoneNumber}
+                  value={props.showShippingPhoneNumber ? props.showShippingPhoneNumber : null}
+                />
+              </div>
+              <div className="dealitem-error-msg" id="shipping-phone-number-error"></div>
+
+
+            </div>
+
+          }
+
+        </form>
       </div>
 
 
