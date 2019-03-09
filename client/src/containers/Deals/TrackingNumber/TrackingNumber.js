f458230d786e310db5968340fcab8ac8d145e7a7
avakrishn <avakrishn@gmail.com>
2019-03-08 18:50:48 -0800

    add mobile css

--- client/src/containers/Deals/TrackingNumber/TrackingNumber.js
+++ client/src/containers/Deals/TrackingNumber/TrackingNumber.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./TrackingNumber.css"
+import "./TrackingNumberMobile.css"
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import { Link } from "react-router-dom";
