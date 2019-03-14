50ce42a189f766d5ec4c413907c8046010f05e99
avakrishn <avakrishn@gmail.com>
2019-03-13 18:31:23 -0700

    update handleLongDescription to handle edge cases

--- client/src/utils/helper_functions.js
+++ client/src/utils/helper_functions.js
@@ -1,8 +1,11 @@
 export function handleLongDescription(description, maxChar, index) {
   let trimmedDescription = description.trim();
-  if (trimmedDescription.length > maxChar) {
+  
+  if (trimmedDescription.length > maxChar && trimmedDescription.indexOf(' ', index) !== -1) {
     return trimmedDescription.substring(0, trimmedDescription.indexOf(' ', index)) + "...";
-  } else {
-    return description
+  } else if(trimmedDescription.length > maxChar){
+    return trimmedDescription.substring(0, index) + "...";
+  }else {
+    return description;
   }
 }
