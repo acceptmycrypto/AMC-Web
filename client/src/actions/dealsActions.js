d07d2bd4264f85a3b783f05dc0ff11cb8de80028
avakrishn <avakrishn@gmail.com>
2019-04-06 23:22:58 -0700

    refactor deals actions

--- client/src/actions/dealsActions.js
+++ client/src/actions/dealsActions.js
@@ -1,3 +1,17 @@
+export const FETCH_DEALS_BEGIN = "FETCH_DEALS_BEGIN";
+export const FETCH_DEALS_SUCCESS= "FETCH_DEALS_SUCCESS";
+export const FETCH_DEALS_FAILURE= "FETCH_DEALS_FAILURE";
+export const CAN_UPDATE_TRACKING_BEGIN= "CAN_UPDATE_TRACKING_BEGIN";
+export const CAN_UPDATE_TRACKING_SUCCESS= "CAN_UPDATE_TRACKING_SUCCESS";
+export const CAN_UPDATE_TRACKING_FAILURE= "CAN_UPDATE_TRACKING_FAILURE";
+export const EDIT_TRACKING_NUMBER= "EDIT_TRACKING_NUMBER";
+export const EDIT_TRACKING_CARRIER= "EDIT_TRACKING_CARRIER";
+export const UPDATE_TRACKING_NUMBER_BEGIN= "UPDATE_TRACKING_NUMBER_BEGIN";
+export const UPDATE_TRACKING_NUMBER_SUCCESS= "UPDATE_TRACKING_NUMBER_SUCCESS";
+export const UPDATE_TRACKING_NUMBER_FAILURE= "UPDATE_TRACKING_NUMBER_FAILURE";
+export const RESET_TRACKING= "RESET_TRACKING";
+
+
 export function _loadDeals(token) {
   const settings = {
     method: "POST",
@@ -21,16 +35,16 @@ export function _loadDeals(token) {
 }
 
 export const fetchDealsBegin = () => ({
-  type: "FETCH_DEALS_BEGIN"
+  type: FETCH_DEALS_BEGIN
 });
 
 export const fetchDealsSuccess = deals => ({
-  type: "FETCH_DEALS_SUCCESS",
+  type: FETCH_DEALS_SUCCESS,
   payload: { deals }
 });
 
 export const fetchDealsFailure = error => ({
-  type: "FETCH_DEALS_FAILURE",
+  type: FETCH_DEALS_FAILURE,
   payload: { error }
 });
 
@@ -60,16 +74,16 @@ export function _canUpdateTracking (token, txn_id, deal_id) {
 }
 
 export const canTrackingBegin = () => ({
-  type: "CAN_UPDATE_TRACKING_BEGIN"
+  type: CAN_UPDATE_TRACKING_BEGIN
 });
 
 export const canTrackingSuccess = res => ({
-  type: "CAN_UPDATE_TRACKING_SUCCESS",
+  type: CAN_UPDATE_TRACKING_SUCCESS,
   payload: res
 });
 
 export const canTrackingFailure = error => ({
-  type: "CAN_UPDATE_TRACKING_FAILURE",
+  type: CAN_UPDATE_TRACKING_FAILURE,
   payload: { error }
 });
 
@@ -77,7 +91,7 @@ export const editTrackingNumber = event => {
   let number = event.target.value.trim();
   
   return{
-    type: "EDIT_TRACKING_NUMBER",
+    type: EDIT_TRACKING_NUMBER,
     payload: number
   }
 };
@@ -85,7 +99,7 @@ export const editTrackingNumber = event => {
 export const editTrackingCarrier = selectedOptions => {
   let carrier = selectedOptions.value;
   return {
-    type: "EDIT_TRACKING_CARRIER",
+    type: EDIT_TRACKING_CARRIER,
     payload: {carrier}
   }
 };
@@ -114,19 +128,19 @@ export function updateTrackingNumber(token, txn_id, trackingNumber, trackingCarr
 }
 
 export const trackingNumberBegin = () => ({
-  type: "UPDATE_TRACKING_NUMBER_BEGIN"
+  type: UPDATE_TRACKING_NUMBER_BEGIN
 });
 
 export const trackingNumberSuccess = res => ({
-  type: "UPDATE_TRACKING_NUMBER_SUCCESS",
+  type: UPDATE_TRACKING_NUMBER_SUCCESS,
   payload: res
 });
 
 export const trackingNumberFailure = error => ({
-  type: "UPDATE_TRACKING_NUMBER_FAILURE",
+  type: UPDATE_TRACKING_NUMBER_FAILURE,
   payload: { error }
 });
 
 export const resetTracking = () => ({
-  type: "RESET_TRACKING"
+  type: RESET_TRACKING
 });
