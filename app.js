3570067207507b62934a17b9fee37145a13473cf
Simon Nguyen <simonnguyen3054@gmail.com>
2019-04-15 07:47:21 -0700

    remove beta and unused routes

--- app.js
+++ app.js
@@ -31,9 +31,6 @@ app.use(function(req, res, next) {
 
 //routers
 const navbarRoutes = require("./routes/navbar.js");
-const adminRoutes = require("./routes/admin.js");
-const cryptoRoutes = require("./routes/crypto.js");
-const apiRoutes = require("./routes/api.js");
 const supportRoutes = require("./routes/support.js");
 const userProfileRoutes = require("./routes/user_profile.js");
 const friendProfileRoutes = require("./routes/friend_profile.js");
@@ -51,9 +48,6 @@ const chatRoutes = require("./routes/chat.js");
 const homepageRoutes = require("./routes/homepage.js");
 
 app.use("/", navbarRoutes);
-app.use("/", adminRoutes);
-app.use("/", cryptoRoutes);
-app.use("/", apiRoutes);
 app.use("/", supportRoutes);
 app.use("/", userProfileRoutes);
 app.use("/", friendProfileRoutes);
